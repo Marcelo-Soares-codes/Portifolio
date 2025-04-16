60da7b2436659f53189f03fcaee6e4b9c32de117
Marcelo Soares <marcelo.soares1070@gmail.com>
2025-04-16 17:38:54 -0300

    Revert "fix vite base"
    This reverts commit 11d4848743a3f89fc3bf0973d75457a0f09b068f.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portifolio/', // 👈 nome exato do repositório no GitHub
 });
